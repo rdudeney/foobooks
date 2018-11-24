90fd10fe13444d58543cd2ac13b8b6803ca9db06
Rupert <rupert.dudeney@gmail.com>
2018-11-24 05:30:39 -0500

    Progress from week 11

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -2,8 +2,76 @@
 namespace App\Http\Controllers;
     use Illuminate\Http\Request;
     use IanLChapman\PigLatinTranslator\Parser;
+    use App\Book;
 class PracticeController extends Controller
 {
+
+    public function practice7()
+    {
+        # First get a book to delete
+        $book = Book::where('author', '=', 'F. Scott Fitzgerald')->first();
+
+        if (!$book) {
+            dump('Did not delete- Book not found.');
+        } else {
+            $book->delete();
+            dump('Deletion complete; check the database to see if it worked...');
+        }
+    }
+
+    public function practice6()
+    {
+        # First get a book to update
+        $book = Book::where('author', '=', 'F. Scott Fitzgerald')->first();
+
+        if (!$book) {
+            dump("Book not found, can't update.");
+        } else {
+            # Change some properties
+            $book->title = 'The Really Great Gatsby';
+            $book->published_year = '2025';
+
+            # Save the changes
+            $book->save();
+
+            dump('Update complete; check the database to confirm the update worked.');
+        }
+    }
+
+    public function practice5()
+    {
+        $book = new Book();
+        $books = $book->where('title', 'LIKE', '%Harry Potter%')->get();
+
+        if ($books->isEmpty()) {
+            dump('No matches found');
+        } else {
+            foreach ($books as $book) {
+                dump($book->title);
+            }
+        }
+    }
+
+    public function practice4()
+    {
+        # Instantiate a new Book Model object
+        $book = new Book();
+
+        # Set the properties
+        # Note how each property corresponds to a field in the table
+        $book->title = 'Harry Potter and the Sorcerer\'s Stone';
+        $book->author = 'J.K. Rowling';
+        $book->published_year = 1997;
+        $book->cover_url = 'http://prodimage.images-bn.com/pimages/9780590353427_p0_v1_s484x700.jpg';
+        $book->purchase_url = 'http://www.barnesandnoble.com/w/harry-potter-and-the-sorcerers-stone-j-k-rowling/1100036321?ean=9780590353427';
+
+        # Invoke the Eloquent `save` method to generate a new row in the
+        # `books` table, with the above data
+        $book->save();
+
+        dump('Added: '.$book->title);
+    }
+
     /**
      * Demonstrating using an external package
      */
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,45 @@
 |
 */
 
+Route::get('/debug', function () {
+
+    $debug = [
+        'Environment' => App::environment(),
+    ];
+
+    /*
+    The following commented out line will print your MySQL credentials.
+    Uncomment this line only if you're facing difficulties connecting to the
+    database and you need to confirm your credentials. When you're done
+    debugging, comment it back out so you don't accidentally leave it
+    running on your production server, making your credentials public.
+    */
+    #$debug['MySQL connection config'] = config('database.connections.mysql');
+
+    try {
+        $databases = DB::select('SHOW DATABASES;');
+        $debug['Database connection test'] = 'PASSED';
+        $debug['Databases'] = array_column($databases, 'Database');
+    } catch (Exception $e) {
+        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
+    }
+
+    dump($debug);
+});
+
 Route::get('/', 'WelcomeController');
+
+# /routes/web.php
+Route::get('/books/search', 'BookController@search'); # <-- NEW 1 of 2
+Route::get('/books/search-process', 'BookController@searchProcess'); # <-- NEW 2 of 2
+
+Route::get('/books/create', 'BookController@create');
+Route::post('/books', 'BookController@store');
 
 Route::get('/books/{title}', 'BookController@show');
 Route::get('/books', 'BookController@index');
+
+Route::any('/practice/{n?}', 'PracticeController@index');
 
 /*
  * Pages
@@ -23,5 +58,31 @@ Route::get('/books', 'BookController@index');
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
 
-Route::any('/practice/{n?}', 'PracticeController@index');
+Route::get('/debug', function () {
+
+    $debug = [
+        'Environment' => App::environment(),
+    ];
+
+    /*
+    The following commented out line will print your MySQL credentials.
+    Uncomment this line only if you're facing difficulties connecting to the
+    database and you need to confirm your credentials. When you're done
+    debugging, comment it back out so you don't accidentally leave it
+    running on your production server, making your credentials public.
+    */
+    #$debug['MySQL connection config'] = config('database.connections.mysql');
+
+    try {
+        $databases = DB::select('SHOW DATABASES;');
+        $debug['Database connection test'] = 'PASSED';
+        $debug['Databases'] = array_column($databases, 'Database');
+    } catch (Exception $e) {
+        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
+    }
+
+    dump($debug);
+});
+
+
 
